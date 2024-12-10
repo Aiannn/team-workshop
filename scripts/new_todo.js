ce5d84985e632f507f7583a757f6cd86d7362b7a
Aiannn <soktoevayan@gmail.com>
2024-12-10 22:27:50 +0700

    complete new_todo

---
+++ scripts/new_todo.js
@@ -0,0 +1,63 @@
+function getUsers() {
+    fetch('http://localhost:8083/api/users')
+    .then(response => response.json())
+    .then(data => {
+        putUsersToDropdown(data)
+    })
+}
+
+function getCategories() {
+    fetch('http://localhost:8083/api/categories')
+    .then(response => response.json())
+    .then(data => {
+        putCategoriesToDropdown(data)
+    })
+}
+
+function putUsersToDropdown(users) {
+    let usersDropdown = document.querySelector('#users-list-select')
+    for (user of users) {
+        let option = new Option(user.name, user.id)
+        usersDropdown.append(option)
+    }
+}
+
+function putCategoriesToDropdown(categories) {
+    let categoriesDropdown = document.querySelector('#categories-list-select')
+    for (category of categories) {
+        let option = new Option(category.name, category.name)
+        categoriesDropdown.append(option)
+    }
+} 
+
+function postNewToDo() {
+    let usersDropdown = document.querySelector('#users-list-select').value
+    let categoriesDropdown = document.querySelector('#categories-list-select').value
+    let urgencyDropdown = document.querySelector('#urgency-list-select').value
+    let descriptionTextarea = document.querySelector('#description').value
+    let deadlineText = document.querySelector('#deadline-text').value
+
+    let todoData = {
+        userid: usersDropdown,
+        category: categoriesDropdown,
+        description: descriptionTextarea,
+        deadline: deadlineText,
+        priority: urgencyDropdown
+    }
+
+    fetch('http://localhost:8083/api/todos', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(todoData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("Success: ", data)
+        console.log("Success: ", data)
+    })
+}
+
+getUsers()
+getCategories()
